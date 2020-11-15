f1ca2daa3f7d532bcf6711f8d177908cd28fba8a
Stefan Nožinić <stefan@lugons.org>
2020-11-15 22:55:38 +0100

    Add tests

--- conal/components/activity_manager/test/main.cpp
+++ conal/components/activity_manager/test/main.cpp
@@ -1,9 +1,10 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"  // Brings in Google Mock.
+#include <node_spec/Parser.hpp>
 
-
-TEST(TestSuiteName, TestName) {
-
+TEST(TestParser, TestBasic) {
+  ::conal::activity_manager::node_spec::Parser p; 
+  EXPECT_EQ(p.parse("*,*")->dump(), std::string("*,*"));
 }
 
 int main(int argc, char **argv) {
