4c965d614e949d979a747bbeb517edc1af942172
Stefan Nožinić <stefan@lugons.org>
2020-12-05 15:31:48 +0100

    CppLoader now has special support for conal compatibility

--- conal/components/code_manager/loaders/CppLoader/CppLoader.cpp
+++ conal/components/code_manager/loaders/CppLoader/CppLoader.cpp
@@ -9,6 +9,11 @@
 
 #include <sstream>
 #include <chrono>
+#include <thread>
+
+#include <dlfcn.h>
+#include <iostream>
+
 
 using namespace conal::code_manager;
 
@@ -17,7 +22,7 @@ std::string get_random_executable_name() {
     std::stringstream ss; 
     ss << std::chrono::duration_cast<std::chrono::seconds>(
                    p1.time_since_epoch()).count() ;
-    return "/tmp/conal-code-manager-cpploader-output-"+ss.str();
+    return "/tmp/conal-code-manager-cpploader-output-"+ss.str() + ".so";
 }
 
 std::string decode64(const std::string &val) {
@@ -42,10 +47,43 @@ bool CppLoader::validate(std::string path, std::vector<std::string> params, EnvP
     return ! findToolChain(env).empty()
         && fs::path(path).extension() == ".cpp";
 }
+
+std::string read_file(std::string path) {
+    std::fstream reader(path);
+    std::stringstream ss; 
+    while (reader.good()) {
+        std::string line;
+        std::getline(reader, line);
+        ss << line << std::endl;
+    }
+    reader.close();
+    return ss.str();
+}
+
+std::string conal_entry_function = "___conal_entry_cpp";
+
 std::string CppLoader::load(std::string path, std::vector<std::string> params, EnvParams env) {
     auto toolchainPath = findToolChain(env);
     auto execName = get_random_executable_name();
-    std::string command = toolchainPath + " " + path + " " + "-o " + execName;
+    std::string source_path = get_random_executable_name() + ".cpp";
+    
+    std::fstream source_output(source_path, std::ios::out);
+    source_output 
+        << read_file(path)
+        << std::endl
+        << "#include <vector>\n"
+        << "#include <DataBinding.hpp>\n"
+        << "typedef int (*entry_start_pointer_t)(std::vector<::conal::code_manager::DataBinding>,std::vector<::conal::code_manager::DataBinding>);\n"
+        << "extern \"C\" " 
+        << "entry_start_pointer_t " 
+        << conal_entry_function << "()" 
+        << "{"
+        << "return start;"
+        << "}"
+        << std::endl;
+    source_output.close();
+
+    std::string command = toolchainPath + " " + source_path + " -shared " + "-o " + execName;
     int status = system(command.c_str());
     if (status != 0) return "";
     std::string output;
@@ -65,15 +103,38 @@ void CppLoader::run(std::string code, std::vector<DataBinding> in, std::vector<D
     executableOutput.write(result.data(), result.size());
     executableOutput.close();
     std::string command1 = "chmod +x " + execName;
-    system(command1.c_str());
-    system(execName.c_str());
+    void* handle = dlopen(execName.c_str(), RTLD_LAZY);
+    const char *dlsym_error = dlerror();
+    if (!handle) {
+        std::cout << "Error while loading file: " << execName << std::endl;
+        std::cout << dlsym_error << std::endl;
+        return;
+    }
+    typedef int (*entry_start_pointer_t)(std::vector<::conal::code_manager::DataBinding>,std::vector<::conal::code_manager::DataBinding>);
+    
+    typedef entry_start_pointer_t (*entry_function)();
+    entry_function entry_symbol = (entry_function) dlsym(handle, conal_entry_function.c_str());
+    dlsym_error = dlerror();
+    if (dlsym_error) {
+            std::cout << dlsym_error << std::endl;
+    }
+    entry_start_pointer_t start_function = entry_symbol();
+    std::thread process([start_function, in, out] () {
+        int result = start_function(in, out);
+    });
+    process.detach();
+    
 }
 
 std::string CppLoader::findToolChain(EnvParams env) {
     std::string conalDirectory = std::string(std::getenv("CONAL_DIR")); 
-    if (env["ARCH"] == "x86_64" && fs::exists("/usr/bin/g++")) return "/usr/bin/g++";
+    std::string flags =  " -L" + conalDirectory + "/sdk/lib/"
+            + " -lConalCodeManager -lConalFramework"
+            + " -I" + conalDirectory + "/sdk/include/"
+            + " -fPIC";
+    if (env["ARCH"] == "x86_64" && fs::exists("/usr/bin/g++")) return "/usr/bin/g++ " + flags;
     if (fs::exists("/usr/bin/" + env["ARCH"] + "-linux-gnu-g++")) {
-        return "/usr/bin/" + env["ARCH"] + "-linux-gnu-g++";
+        return "/usr/bin/" + env["ARCH"] + "-linux-gnu-g++ " + flags;
     }
     return "";
 }
